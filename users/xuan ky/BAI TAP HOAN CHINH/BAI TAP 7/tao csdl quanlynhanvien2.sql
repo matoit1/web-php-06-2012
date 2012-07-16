f68e54ceba720b653d3173016392a40339e9cdf0
ducthienkhtn <ducthienkhtn@b1bf1457-cbaa-2048-4bf9-74360b1033a4>
2012-07-16 12:23:22 +0000

    sua bai sql query

--- users/xuan ky/BAI TAP HOAN CHINH/BAI TAP 7/tao csdl quanlynhanvien2.sql
+++ users/xuan ky/BAI TAP HOAN CHINH/BAI TAP 7/tao csdl quanlynhanvien2.sql
@@ -1,5 +1,5 @@
 ﻿-- cau lenh tao co so du lieu
-create database quanlynhanvien2
+--create database quanlynhanvien2
 
 -- cau lenh tao bang 'phongban' cho CSDL, primary key = maphg(bang ma utf8)
 create table quanlynhanvien2.phongban 
@@ -31,7 +31,7 @@ create table quanlynhanvien2.nhanvien
 	phai		varchar(3),
 	luong		float,
 	ma_nql		varchar(9),
-	pgh			int,
+	phg			int,
 	constraint pk_nhanvien primary key (manv)
 )engine = InnoBD default charset = utf8 collate = utf8_unicode_ci;
 
@@ -69,13 +69,13 @@ create table quanlynhanvien2.phancong
 -- theo so do lien he giu cac bang, lan luot dung cac lenh sau de tao khoa ngoai bang cau lenh
 
 -- 1. tao khoa ngoai cho hai bang 'phongban' va 'ddiem_phg' (quan he 1 - n)
-alter table ddiem_phg add constraint fk_phongban_ddiem_phg foreign key (maphg) references phongban(maphg)
+alter table diadiem_phg add constraint fk_phongban_ddiem_phg foreign key (maphg) references phongban(maphg)
 
 -- 2. tao khoa ngoai cho 2 bang 'nhanvien - phong ban' (quan he n-1)
-alter table nhanvien add constraint fk_nhanvien_phongbna foreign key (maphg) references phongban(maphg)
+alter table nhanvien add constraint fk_nhanvien_phongban foreign key (phg) references phongban(maphg)
 
 -- 3. tao khoa ngoai cho 2 bang 'nhan vien - phong ban (quan he 1 - n) can hoi thay lai (ma_nql hay manv)
-alter table phongban add constraint fk_nhanvien_phongban foreign key (trphg) references nhanvien(manv)
+alter table phongban add constraint fk_phongban_nhanvien foreign key (trphg) references nhanvien(manv)
 
 -- 4. tao khoa ngoai cho 2 bang 'thannhan - nhanvien' (quan he 1-n) 
 alter table thannhan add constraint fk_thannhan_nhanvien foreign key (ma_nvien) references nhanvien(manv)
@@ -87,15 +87,16 @@ alter table phancong add constraint fk_phancong_nhanvien foreign key (ma_nvien) 
 alter table phancong add constraint fk_phancong_dean foreign key (soda) references dean(mada)
 
 -- 7. tao khoa noi bo cho bang nhanvien
-alter table nhanvien add constraint fk_nhanvien_nhanvien foreign key (manv) references nhanvien(ma_nql)
+alter table nhanvien add constraint fk_nhanvien_nhanvien foreign key (ma_nql) references nhanvien(manv)
+
 
 -- NHAP LIEU CHO TUNG BANG
 -- 1. nhap lieu cho bang 'phongban'
 insert into quanlynhanvien2.phongban (tenphg, maphg, trphg, ng_nhanchuc) values
-	('Quản Lý',1,'666884444','1971-06-19'),
-	('Tổ Chức',2,'987987987','2001-01-15'),
-	('Điều Hành',4,'123456789','1985-01-01'),
-	('Nghiên Cứu',5,'333445555','1978-05-22')
+	('Quản Lý',1,NULL,'1971-06-19'),
+	('Tổ Chức',2,NULL,'2001-01-15'),
+	('Điều Hành',4,NULL,'1985-01-01'),
+	('Nghiên Cứu',5,NULL,'1978-05-22')
 
 -- 2. nhap lieu cho bang 'diadiem_phg'
 insert into quanlynhanvien2.diadiem_phg (maphg, diadiem) values	
@@ -108,15 +109,15 @@ insert into quanlynhanvien2.diadiem_phg (maphg, diadiem) values
 	
 -- 3 Nhap lieu cho bang 'nhanvien'
 insert into quanlynhanvien2.nhanvien (honv, tenlot, tennv, manv, ngsinh, dchi, phai, luong, ma_nql, phg) values
-	('Cao','Si','Ki','123123456','1986-08-09','123 Lê Lợi - Quận 1 - TP HCM','Nam',15000,'123456789',5),
-	('Đinh','Bá','Tiến','123456789','1955-01-09','731 Trần Hưng Đạo - Quận 1 - TP HCM','Nam',30000,'123456789',5),
-	('Trương','Lê','Đoan','147852369','1986-05-15','22/41/1 Lê Văn Thọ- Phường 11 - Gò Vấp','Nữ',16500,'147852369',1),
-	('Nguyễn','Thanh','Tùng','333445555','1945-12-08','638 Nguyễn Văn Cừ - Quận 5 - TP HCM','Nam',40000,'123456789',5),
-	('Lê','Huỳnh','Đức','666884444','1986-06-01','126 Nguyễn Văn Cừ - Quận 5 - TP HCM','Nam',27000,'666884444',1),
-	('Vương','Ngọc','Quyên','888665555','1965-09-15','414 Nơ Trang Long - Quận Bình Thạnh - TP HCM','Nam',29400,'999887777',4),
-	('Lê','Thị','Nhàn','987654321','1931-06-20','291 Hồ Văn Huê - Quận Phú Nhuận - TP HCM','Nữ',43000,'123456789',5),
-	('Võ','Đức','Minh','987987987','1985-09-16','414 Nơ Trang Long - Quận Bình Thạnh - TP HCM','Nam',30000,'123456789',5),
-	('Bùi','Thụy','Vũ','999887777','1958-07-19','332 Nguyễn Thái Học - Quận 1 - TP HCM','Nam',25000,'123456789',4)
+	('Cao','Si','Ki','123123456','1986-08-09','123 Lê Lợi - Quận 1 - TP HCM','Nam',15000,NULL,5),
+	('Đinh','Bá','Tiến','123456789','1955-01-09','731 Trần Hưng Đạo - Quận 1 - TP HCM','Nam',30000,NULL,5),
+	('Trương','Lê','Đoan','147852369','1986-05-15','22/41/1 Lê Văn Thọ- Phường 11 - Gò Vấp','Nữ',16500,NULL,1),
+	('Nguyễn','Thanh','Tùng','333445555','1945-12-08','638 Nguyễn Văn Cừ - Quận 5 - TP HCM','Nam',40000,NULL,5),
+	('Lê','Huỳnh','Đức','666884444','1986-06-01','126 Nguyễn Văn Cừ - Quận 5 - TP HCM','Nam',27000,NULL,1),
+	('Vương','Ngọc','Quyên','888665555','1965-09-15','414 Nơ Trang Long - Quận Bình Thạnh - TP HCM','Nam',29400,NULL,4),
+	('Lê','Thị','Nhàn','987654321','1931-06-20','291 Hồ Văn Huê - Quận Phú Nhuận - TP HCM','Nữ',43000,NULL,5),
+	('Võ','Đức','Minh','987987987','1985-09-16','414 Nơ Trang Long - Quận Bình Thạnh - TP HCM','Nam',30000,NULL,5),
+	('Bùi','Thụy','Vũ','999887777','1958-07-19','332 Nguyễn Thái Học - Quận 1 - TP HCM','Nam',25000,NULL,4)
 	
 -- 4 Nhap lieu cho bang 'dean'
 insert into quanlynhanvien2.dean (tenda, mada, ddiem_da, phong)values	
@@ -127,7 +128,7 @@ insert into quanlynhanvien2.dean (tenda, mada, ddiem_da, phong)values
 	('Hóa Học', 40, 'Biên Hòa', 5)
 	
 -- 5 Nhap lieu cho bang 'phancong'
-insert into quanlynhanvien2.phancong (ma_nvien, soda)values	
+insert into quanlynhanvien2.phancong(ma_nvien, soda, thoigian)values	
 	('123123456', 2, 1),
 	('123123456', 40, 2),
 	('123456789', 40, 3),
@@ -141,13 +142,26 @@ insert into quanlynhanvien2.phancong (ma_nvien, soda)values
 	('999887777', 40, 11)
 	
 -- 6 Nhap lieu cho bang 'thannhan'
-insert into (ma_nvien, tentn, phai, ngsinh, quanhe)values	
+insert into thannhan(ma_nvien, tentn, phai, ngsinh, quanhe) values	
 	('333445555','Châu Long','Nam','2005-01-02','Chồng'),
 	('333445555','Khang','Nam','1973-10-25','Con Trai'),
 	('333445555','Ngọc Phi','Nữ','1976-04-05','Con Gái'),
 	('987654321','Đăng','Nam','1932-02-29','Chồng'),
 	('999887777','Toàn','Nam','1986-01-02','Chồng')
 	
+	
+/* Cap nhat thong tin field TrPHG cho table PhongBan thoa rang buoc khoa ngoai */
+update phongban set trphg='666884444' where maphg=1
+update phongban set trphg='987987987' where maphg=2
+update phongban set trphg='123456789' where maphg=4
+update phongban set trphg='333445555' where maphg=5
 
+/* Cap nhat thong tin field Ma_NQL cho table NhanVien thoa rang buoc khoa ngoai */
+update nhanvien set ma_nql='123456789' where manv='123123456'
+--
+--
+
+/* Xoa 1 record trong table PhanCong: khong cho nhan vien Bui Thuy Vu tham gia du an Hoa Hoc nua*/
+delete from phancong where ma_nvien='999887777' and soda=40
 
 	
